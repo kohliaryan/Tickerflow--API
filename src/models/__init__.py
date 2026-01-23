8048c6ee09b83019cdf1f4fd46ee990fc9d0fc32
kohliaryan <kohliaryan2004@gmail.com>
2026-01-23 18:10:33 +0530

    feat: fix circular import issue

---
+++ src/models/__init__.py
@@ -0,0 +1,3 @@
+from .users import User, Role
+from .events import Event
+from .bookings import Booking
--- src/models/users.py
+++ src/models/users.py
@@ -1,25 +1,22 @@
 from datetime import datetime
-
 from pydantic import EmailStr
 from sqlalchemy import String, DateTime, func, Table, Column, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.database.database import Base
 
-user_roles=Table(
+user_roles = Table(
     "user_roles",
     Base.metadata,
     Column("user_id", ForeignKey("users.id"), primary_key=True),
-    Column("role_id", ForeignKey("roles.id"), primary_key=True)
+    Column("role_id", ForeignKey("roles.id"), primary_key=True),
 )
 
-
 class User(Base):
-
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True)
-    email: Mapped[EmailStr] = mapped_column(String, unique=True, nullable=False, index=True)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    email: Mapped[EmailStr] = mapped_column(String, unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String, nullable=False)
 
     roles: Mapped[list["Role"]] = relationship(
@@ -28,23 +25,25 @@ class User(Base):
         lazy="selectin",
     )
 
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    bookings: Mapped[list["Booking"]] = relationship(
+        back_populates="user"
+    )
 
-    def __repr__(self):
-        return f"<User(id={self.id}, email={self.email})>"
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+
 
 class Role(Base):
-
     __tablename__ = "roles"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
-    name: Mapped[str] = mapped_column(String, unique=True, nullable=False, index=True)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
 
     users: Mapped[list["User"]] = relationship(
-        secondary=user_roles,
+        secondary="user_roles",
         back_populates="roles",
         lazy="selectin",
     )
-
-    def __repr__(self):
-        return f"<Role(id={self.id}, name={self.name})>"
