d2bc4d13943494f11d9c13e45cbb85c58c5f6ef1
kohliaryan <kohliaryan2004@gmail.com>
2026-01-25 13:06:24 +0530

    feat: add seed file for booking in main

--- src/main.py
+++ src/main.py
@@ -2,15 +2,23 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from src.database.database import engine, Base
+from src.database.database import engine, Base, AsyncSessionLocal
 from src.routers.auth import auth_router
 from src.routers.events import event_router
+from src.scripts.seed_bookings import seed_bookings
+from src.scripts.seed_events import seed_events
+from src.scripts.seed_users import seed_users
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+    async with AsyncSessionLocal() as session:
+        await seed_users(session)
+        await seed_events(session)
+        await seed_bookings(session)
 
     yield
 
