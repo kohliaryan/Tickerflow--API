5a89c246a5177a5dc406c9969a3d48cb50cbcd16
kohliaryan <kohliaryan2004@gmail.com>
2026-01-25 13:05:39 +0530

    fix: map email to str instead of EmailStr

--- src/models/users.py
+++ src/models/users.py
@@ -16,7 +16,7 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    email: Mapped[EmailStr] = mapped_column(String, unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String, nullable=False)
 
     roles: Mapped[list["Role"]] = relationship(
